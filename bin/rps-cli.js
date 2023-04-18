c58b0fdb79cc0caa5657e28fa96cde082cae85be
meilinru <77815163+meilinru@users.noreply.github.com>
2023-04-18 13:02:15 -0400

    maybe itll work

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -42,17 +42,10 @@ if (res === undefined) {
                           e.g. {"player":"rock"}
           node-rps rock   Return JSON with results for RPS played against a simulated opponent.
                           e.g {"player":"rock","opponent":"scissors","result":"win"}`);
-    console.log(`Usage: node-rps [SHOT]
-        Play Rock Paper Scissors (RPS)
-        
-          -h, --help      display this help message and exit
-          -r, --rules     display the rules and exit
-        
-        Examples:
-          node-rps        Return JSON with single player RPS result.
-                          e.g. {"player":"rock"}
-          node-rps rock   Return JSON with results for RPS played against a simulated opponent.
-                          e.g {"player":"rock","opponent":"scissors","result":"win"}`);
+    console.log(`Rules for Rock Paper Scissors:
+    - Scissors CUTS Paper
+    - Paper COVERS Rock
+    - Rock CRUSHES Scissors`); 
     process.exit(1);
 } else {
 	console.log(JSON.stringify(res));
