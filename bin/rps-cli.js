bd38e0df4671ae5af76c1654d42c903b211c09e9
meilinru <77815163+meilinru@users.noreply.github.com>
2023-04-20 12:35:38 -0400

    Update rps-cli.js

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,49 +1,39 @@
 #!/usr/bin/env node
-
-import {rps} from "../lib/rpsls.js"
 import minimist from 'minimist';
+import { rps } from "../lib/rpsls.js"
 
-var arg = minimist(process.argv.slice(2));
+var args = minimist(process.argv.slice(2));
 
-if (arg.h || arg.help) {
-	help();
-	process.exit();
+const help_message = `Usage: node-rps [SHOT]
+Play Rock Paper Scissors (RPS)
+  -h, --help      display this help message and exit
+  -r, --rules     display the rules and exit
+Examples:
+  node-rps        Return JSON with single player RPS result.
+                  e.g. {"player":"rock"}
+  node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                  e.g {"player":"rock","opponent":"scissors","result":"win"}`; 
+
+const rules = `Rules for Rock Paper Scissors:
+  - Scissors CUTS Paper
+  - Paper COVERS Rock
+  - Rock CRUSHES Scissors`; 
+
+if (args.h || args.help) {
+	console.log(help_message);
+	process.exit(0);
 }
 
-if (arg.r || arg.rules) {
-	rules();
-	process.exit();
+if (args.r || args.rules) {
+	console.log(rules);
+	process.exit(0);
 }
+
+let shot = args._[0]
 
 try {
-	console.log(JSON.stringify(rps(arg._[0])));
-} catch (error) {
-	if (error instanceof RangeError) {
-		console.log('Input is not in range');
-		rules();
-		process.exit();
-	}
+	console.log(JSON.stringify(rps(shot)));
+} catch (e) {
+	console.log(help_message);
+	console.log(rules);
 }
-
-function help() {
-   console.log(
-     'Usage: node-rps [SHOT]
-     Play Rock Paper Scissors (RPS)
-
-	-h, --help      display this help message and exit
-	-r, --rules     display the rules and exit
-
-	Examples:
-	node-rps        Return JSON with single player RPS result.
-			e.g. {"player":"rock"}
-	node-rps rock   Return JSON with results for RPS played against a simulated opponent.
-			e.g {"player":"rock","opponent":"scissors","result":"win"}`
-	);
-
-function rules(){
-    console.log(
-        `Rules for Rock Paper Scissors:
-        - Scissors CUTS Paper
-        - Paper COVERS Rock
-        - Rock CRUSHES Scissors`
-    );
