77b1a2dd723172e160d835cc5b51db09cf8822dd
meilinru <77815163+meilinru@users.noreply.github.com>
2023-04-25 22:44:49 -0400

    Update rpsls-cli.js

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -5,18 +5,22 @@ import { rpsls } from "../lib/rpsls.js"
 
 var args = minimist(process.argv.slice(2))
 
-const help_message = `Usage: node-rpsls [SHOT]
+const help_message = 
+`Usage: node-rpsls [SHOT]
 Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
+
   -h, --help        display this help message and exit
   -r, --rules       display the rules and exit
+  
 Examples:
   node-rpsls        Return JSON with single player RPSLS result.
                     e.g. {"player":"rock"}
   node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
-                    e.g {"player":"rock","opponent":"Spock","result":"lose"}
-`;
+                    e.g {"player":"rock","opponent":"Spock","result":"lose"}`;
 
-const rules = `Rules for the Lizard-Spock Expansion of Rock Paper Scissors:
+const rules = 
+`Rules for the Lizard-Spock Expansion of Rock Paper Scissors:
+
 - Scissors CUTS Paper
 - Paper COVERS Rock
 - Rock SMOOSHES Lizard
@@ -26,8 +30,7 @@ const rules = `Rules for the Lizard-Spock Expansion of Rock Paper Scissors:
 - Lizard EATS Paper
 - Paper DISPROVES Spock
 - Spock VAPORIZES Rock
-- Rock CRUSHES Scissors
-`;
+- Rock CRUSHES Scissors`;
 
 if (args.h || args.help) {
         console.log(help_message);
@@ -43,9 +46,7 @@ let shot = args._[0]
 
 try {
         console.log(JSON.stringify(rpsls(shot)));
-} catch (error) {
-        if (error instanceof RangeError) {
-        console.log(`Error: ${args._[0]} is not in the acceptable range.`);
+} catch (e) {
         console.log(rules);
         process.exit();
     }
