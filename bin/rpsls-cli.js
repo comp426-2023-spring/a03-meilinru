9d4bad1ccebae74925e33696f2c7dba7dfae62d3
meilinru <77815163+meilinru@users.noreply.github.com>
2023-04-25 22:58:31 -0400

    Update rpsls-cli.js

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -3,23 +3,21 @@
 import minimist from 'minimist';
 import { rpsls } from "../lib/rpsls.js"
 
-var args = minimist(process.argv.slice(2))
+const args = minimist(process.argv.slice(2));
 
-const help_message = 
-`Usage: node-rpsls [SHOT]
+const help = 'Usage: node-rpsls [SHOT]
 Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
-
-  -h, --help        display this help message and exit
-  -r, --rules       display the rules and exit
-  
+    -h, --help        display this help message and exit
+    -r, --rules       display the rules and exit
 Examples:
-  node-rpsls        Return JSON with single player RPSLS result.
+    node-rpsls        Return JSON with single player RPSLS result.
                     e.g. {"player":"rock"}
-  node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
-                    e.g {"player":"rock","opponent":"Spock","result":"lose"}`;
+    node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
+                    e.g {"player":"rock","opponent":"Spock","result":"lose"}';
+
 
 const rules = 
-`Rules for the Lizard-Spock Expansion of Rock Paper Scissors:
+'Rules for the Lizard-Spock Expansion of Rock Paper Scissors:
 
 - Scissors CUTS Paper
 - Paper COVERS Rock
@@ -30,16 +28,16 @@ const rules =
 - Lizard EATS Paper
 - Paper DISPROVES Spock
 - Spock VAPORIZES Rock
-- Rock CRUSHES Scissors`;
+- Rock CRUSHES Scissors';
 
 if (args.h || args.help) {
         console.log(help_message);
-        process.exit();
+        process.exit(0);
 }
 
 if (args.r || args.rules) {
         console.log(rules);
-        process.exit();
+        process.exit(0);
 }
 
 let shot = args._[0]
@@ -47,8 +45,7 @@ let shot = args._[0]
 try {
         console.log(JSON.stringify(rpsls(shot)));
 } catch (e) {
+      console.log(help_message);
         console.log(rules);
         process.exit();
-    }
-}
-
+ }
